ae12a8e96c8ac087ede4f60121b70963ab236a14
Rinor <rinor.ahmeti@studentet.uni-pr.edu>
2020-03-23 14:50:04 +0100

    Impelemtimi i disa funksioneve ne server

--- FIEK-TCP-Server.py
+++ FIEK-TCP-Server.py
@@ -1,1 +1,65 @@
-print('test')
+import numpy as np
+import socket 
+import datetime
+
+def IPADDRESS():
+    return f'IP Adresa e juaj eshte: {address[0]}'
+
+def PORT():
+    return f'PORTI I KLIENTIT ESHTE: {address[1]}'
+
+def COUNT(fjala):
+    bashketingulloret = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','w','x','z']
+    zanoret=['a','e','i','o','u','y']
+    cons=0
+    vow=0
+    for i in fjala.lower():
+        if i in bashketingulloret:
+            vow=vow+1
+        elif i in zanoret: 
+            cons=cons+1   
+    return f'Ne fjalen {fjala} kemi {vow} bashketingullore dhe {cons} zanore'
+
+def REVERSE(fjala): 
+    fjala2 = ''
+    for i in fjala:
+        fjala2 = fjala2 + i
+    fjala2 = fjala2[::-1]
+    return fjala2
+
+def PALINDROME(fjala):
+    if fjala==REVERSE(fjala):
+        return True 
+    else:
+        return False
+
+def TIME():
+    return datetime.datetime.now().strftime("%H:%M:%S %D")
+
+def GAME():
+    nums = np.random.randint(1,35,5)
+    numrat = []
+    for i in nums:
+        numrat.append(i)
+    return numrat
+
+host = 'localhost'
+port = 13000
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((host,port))
+
+print(f'Serveri jone eshte startuar ne portin: {port}')
+server.listen(10)
+print('Serveri po pret per ndonje kerkese!')
+
+while True:
+    (client,address)=server.accept()
+    print(f'Lidhur ne serverin {address[0]} ne portin {address[1]}')
+    kerkesa = client.recv(1024) 
+    print(f'Kerkesa nga klienti: {kerkesa.decode("utf-8")}')
+    kerkesaMadhe = kerkesa.lower()
+    print(f'PERGJIGJA E SERVERIT: {kerkesaMadhe.decode("utf-8")}')
+    client.send(kerkesaMadhe)
+    client.close()
+    print("U mbyll sesioni")
